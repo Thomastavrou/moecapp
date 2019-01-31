fc5447ed39612002af39a3c2c3ff494b3441bee6
thomas stavrou <thomastavrou@gmail.com>
2019-01-31 13:53:38 +0200

    enw one signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,16 +50,16 @@ export class MyApp {
       // Enable to debug issues:
       // window["plugins"].OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
 
-
-    //   var notificationOpenedCallback = function(jsonData) {
-    //     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-    //   };
-    //
-    //   window["plugins"].OneSignal
-    //     .startInit("050ee4a4-f939-42c5-bf34-f35e395d4afe", "683140671042")
-    //     .handleNotificationOpened(notificationOpenedCallback)
-    //     .endInit();
-    // });
+      //
+      // var notificationOpenedCallback = function(jsonData) {
+      //   console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+      // };
+      //
+      // window["plugins"].OneSignal
+      //   .startInit("050ee4a4-f939-42c5-bf34-f35e395d4afe", "683140671042")
+      //   .handleNotificationOpened(notificationOpenedCallback)
+      //   .endInit();
+    });
   }
 
   openPage(page) {
